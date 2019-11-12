390deb8e9f950953ec0f3190b086c42775bc16ca
Kayla Sween <kaylasween@users.noreply.github.com>
2019-11-11 20:48:54 -0600

    Update plans.js

--- src/templates/plans.js
+++ src/templates/plans.js
@@ -9,9 +9,8 @@ import Content, { HTMLContent } from '../components/Content'
 export const BlogPostTemplate = ({
   content,
   contentComponent,
-  description,
-  tags,
-  title,
+  size,
+  image,
   helmet,
 }) => {
   const PostContent = contentComponent || Content
@@ -23,22 +22,10 @@ export const BlogPostTemplate = ({
         <div className="columns">
           <div className="column is-10 is-offset-1">
             <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
-              {title}
+              {size}
             </h1>
-            <p>{description}</p>
+            <p>{image}</p>
             <PostContent content={content} />
-            {tags && tags.length ? (
-              <div style={{ marginTop: `4rem` }}>
-                <h4>Tags</h4>
-                <ul className="taglist">
-                  {tags.map(tag => (
-                    <li key={tag + `tag`}>
-                      <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ) : null}
           </div>
         </div>
       </div>
@@ -94,9 +81,8 @@ export const pageQuery = graphql`
       html
       frontmatter {
         date(formatString: "MMMM DD, YYYY")
-        title
-        description
-        tags
+        size
+        image
       }
     }
   }
